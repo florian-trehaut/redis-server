38a2736acd62ed9a591e39d501d4ecab09f45edb
Florian TREHAUT <florian.trehaut@hillcode.fr>
2024-04-25 16:44:15 +0700

    handle concurrency

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
 };
 
 fn main() {
@@ -9,22 +10,21 @@ fn main() {
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    match TcpListener::bind("127.0.0.1:6379") {
-        Ok(listener) => {
-            for stream in listener.incoming() {
-                match stream {
-                    Ok(mut stream) => handle_client(&mut stream),
-                    Err(e) => {
-                        println!("error: {}", e);
-                    }
-                }
+    let listener = TcpListener::bind("127.0.0.1:6379").expect("Can't bind to address");
+    let mut threads: Vec<_> = vec![];
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                threads.push(thread::spawn(move || handle_client(&mut stream)));
+            }
+            Err(e) => {
+                println!("error: {}", e);
             }
         }
-        Err(e) => {
-            eprintln!("Can't bind to address");
-            panic!("{e}");
-        }
-    };
+    }
+    for thread in threads {
+        thread.join().expect("Can't join threads");
+    }
 }
 
 fn handle_client(stream: &mut TcpStream) {
@@ -41,44 +41,4 @@ fn handle_client(stream: &mut TcpStream) {
 const PONG: &[u8] = "+PONG\r\n".as_bytes();
 fn ping(stream: &mut TcpStream) {
     stream.write_all(PONG).expect("Can't write PONG to stream");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::io::{Read, Write};
-    use std::net::TcpStream;
-    use std::thread;
-
-    fn test_command(command: &[u8], expected_response: &[u8]) {
-        // Start the server in a new thread
-        thread::spawn(main);
-
-        // Give the server a little time to start
-        thread::sleep(std::time::Duration::from_secs(1));
-
-        // Connect to the server
-        let mut stream = TcpStream::connect("127.0.0.1:6379").unwrap();
-
-        // Send a command
-        stream.write_all(command).unwrap();
-
-        // Read the response
-        let mut buf = [0; 512];
-        let n = stream.read(&mut buf).unwrap();
-
-        // Check that the response is correct
-        assert_eq!(&buf[..n], expected_response);
-    }
-
-    #[test]
-    fn test_ping() {
-        test_command(b"ping\r\n\n\n", b"+PONG\r\n");
-    }
-    #[test]
-    fn test_ping_ping() {
-        test_command(b"ping\r\n\nping\r\n\n", b"+PONG\r\n+PONG\r\n");
-    }
-
-    // Add more tests here...
 }
