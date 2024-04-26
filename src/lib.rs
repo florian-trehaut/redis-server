3146968e77f03a95067eacd887ca1595339ec01a
Florian TREHAUT <florian.trehaut@hillcode.fr>
2024-04-26 13:10:59 +0700

    add set and get

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,36 @@
 use std::{
+    collections::HashMap,
     io::{Read, Write},
     net::TcpStream,
+    sync::{Arc, Mutex},
 };
 
 use resp::{Bulk, BulkString};
 mod resp;
 enum RedisCommands {
-    PING,
-    ECHO(Vec<Bulk>),
-    UNKOWN,
+    Ping,
+    Echo(Vec<Bulk>),
+    Get(String),
+    Set((String, String)),
+    Unknown,
 }
+pub type RedisStore = Arc<Mutex<HashMap<String, String>>>;
 impl RedisCommands {
     fn parse(bulkstring: BulkString) -> RedisCommands {
         match bulkstring.bulks().first().unwrap().data().as_str() {
-            "ping" => RedisCommands::PING,
-            "echo" => RedisCommands::ECHO(bulkstring.bulks()[1..].to_vec()),
-            _ => RedisCommands::UNKOWN,
+            "ping" => RedisCommands::Ping,
+            "echo" => RedisCommands::Echo(bulkstring.bulks()[1..].to_vec()),
+            "get" => RedisCommands::Get(bulkstring.bulks().get(1).unwrap().data()),
+            "set" => RedisCommands::Set((
+                bulkstring.bulks().get(1).unwrap().data(),
+                bulkstring.bulks().get(2).unwrap().data(),
+            )),
+            _ => RedisCommands::Unknown,
         }
     }
 }
 
-pub fn handle_client(stream: &mut TcpStream) {
+pub fn handle_client(stream: &mut TcpStream, store: RedisStore) {
     let mut buf = [0; 512];
     while let Ok(n) = stream.read(&mut buf) {
         if n == 0 {
@@ -28,9 +38,11 @@ pub fn handle_client(stream: &mut TcpStream) {
         }
         let command = BulkString::from_bytes(&buf[..n]);
         match RedisCommands::parse(command) {
-            RedisCommands::PING => ping(stream),
-            RedisCommands::ECHO(message) => echo(stream, &message),
-            RedisCommands::UNKOWN => unimplemented(stream),
+            RedisCommands::Ping => ping(stream),
+            RedisCommands::Echo(message) => echo(stream, &message),
+            RedisCommands::Unknown => unimplemented(stream),
+            RedisCommands::Get(key) => get(stream, key, &store),
+            RedisCommands::Set((key, value)) => set(stream, key, value, &store),
         }
     }
 }
@@ -45,11 +57,25 @@ fn ping(stream: &mut TcpStream) {
         .expect("Can't write PONG to stream");
 }
 fn echo(stream: &mut TcpStream, message: &[Bulk]) {
-    let message: Vec<u8> = message
-        .iter()
-        .flat_map(|bulk| bulk.to_bytes())
-        .collect();
+    let message: Vec<u8> = message.iter().flat_map(|bulk| bulk.to_bytes()).collect();
     stream
         .write_all(&Bulk::from_bytes(&message).to_bytes())
         .expect("Can't write response to stream");
 }
+
+fn set(stream: &mut TcpStream, key: String, value: String, store: &RedisStore) {
+    store.lock().unwrap().insert(key, value);
+    responde(stream, "OK")
+}
+fn get(stream: &mut TcpStream, key: String, store: &RedisStore) {
+    match store.lock().unwrap().get(&key) {
+        Some(value) => responde(stream, value),
+        None => responde(stream, NULL),
+    }
+}
+fn responde(stream: &mut TcpStream, data: &str) {
+    let response = Bulk::from_data(data).to_bytes();
+    stream.write_all(&response).unwrap();
+}
+
+const NULL: &str = "-1";
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 // Uncomment this block to pass the first stage
-use std::{net::TcpListener, thread};
+use std::{
+    collections::HashMap,
+    net::TcpListener,
+    sync::{Arc, Mutex},
+    thread,
+};
 
-use redis_starter_rust::handle_client;
+use redis_starter_rust::{handle_client, RedisStore};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
+    let store: RedisStore = Arc::new(Mutex::new(HashMap::new()));
     // Uncomment this block to pass the first stage
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Can't bind to address");
     let mut threads: Vec<_> = vec![];
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                threads.push(thread::spawn(move || handle_client(&mut stream)));
+                let store_clone = store.clone();
+                threads.push(thread::spawn(move || {
+                    handle_client(&mut stream, store_clone)
+                }));
             }
             Err(e) => {
                 println!("error: {}", e);
